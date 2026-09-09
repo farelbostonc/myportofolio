1d9fce205af86ae1d3fa0c1f1cc2402b3e5df1a4
farelboston <farelboston2007@gmail.com>
2026-09-09 17:18:51 +0700

    Complete Tutorial 2

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Farel Boston Corinthians Nadeak",
+        "npm": "2506548490",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada keamanan siber dan bisnis."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Farel Boston Corinthians Nadeak",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
