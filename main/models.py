9738c985363e1b7d1911f3186d6f5641146be5b2
farelboston <farelboston2007@gmail.com>
2026-09-13 21:09:46 +0700

    Menambahkan model Education di dalam main beserta atributnya

--- main/models.py
+++ main/models.py
@@ -24,3 +24,13 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_year = models.TextField()
+    end_year = models.TextField()
+    def __str__(self):
+        return self.title
